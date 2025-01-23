019bc36c3a3e996fd5e4e03f4f6b39d4fad10c11
lxyesa <liu.xuanyu@esa-n.info>
2025-01-23 19:12:47 +0800

    添加应用程序基本结构和网络管理功能
    在 `App.xaml` 和 `App.xaml.cs` 中设置应用程序的基本结构和启动逻辑。
    添加多个二进制文件作为应用程序图标和启动画面。
    在 `MainWindow.xaml` 中定义主窗口布局，并在 `MainWindow.xaml.cs` 中实现初始化和导航逻辑。
    实现 `NetManager.cs` 以处理与服务器的连接。
    更新项目文件以支持 WinUI，定义应用程序身份和属性。
    实现连接对话框和用户页面的 UI 及逻辑。
    添加应用程序兼容性声明，并更新发布配置文件和启动设置。

---
+++ NDVDemoWinUI/Models/NetManager.cs
@@ -0,0 +1,63 @@
+﻿using DRX.Framework;
+using DRX.Framework.Common.Models;
+using DRXNetwork;
+using Microsoft.Graph.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using DRX.Framework.Common.Args;
+
+namespace NDVDemoWinUI.Models
+{
+    public sealed class NetManager
+    {
+        private static readonly Lazy<NetManager> Lazy =
+            new Lazy<NetManager>(() => new NetManager(), true);
+        private static readonly DrxClient? ClientSocket;
+        public static NetManager Instance => Lazy.Value;
+
+        static NetManager()
+        {
+            ClientSocket = new DrxClient("127.0.0.1", 8463, "ffffffffffffffff");
+            ClientSocket.OnErrorCallback += _clientSocket_OnErrorCallback;
+            ClientSocket.OnReceiveCallback += _clientSocket_OnReceiveCallback;
+            ClientSocket.OnConnectedCallback += _clientSocket_OnConnectedCallback;
+        }
+
+        /* ----------------------- 字段属性 ----------------------- */
+        private static bool _isConnected = false;
+
+        /* ----------------------- 事件处理/声明 ----------------------- */
+
+        private static void _clientSocket_OnErrorCallback(object? sender, DRX.Framework.Common.Args.NetworkEventArgs e)
+        {
+#if DEBUG
+            Logger.Error(e?.Message);
+#endif
+        }
+
+        private static void _clientSocket_OnReceiveCallback(object? sender, DRX.Framework.Common.Args.NetworkEventArgs e)
+        {
+            // throw new NotImplementedException();
+        }
+        private static void _clientSocket_OnConnectedCallback(object? sender, NetworkEventArgs e)
+        {
+#if DEBUG
+            Logger.Debug("已经成功连接到服务器");
+#endif
+            _isConnected = true;    // 连接成功, 设置连接状态
+        }
+
+        /* ----------------------- 公开方法 ----------------------- */
+        public void Connect()
+        {
+            ClientSocket?.Connect();
+        }
+
+        public bool IsConnected() => _isConnected;
+        public DrxClient? GetClient() => ClientSocket;
+    }
+}
